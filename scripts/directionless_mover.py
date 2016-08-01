6117517907f1500e9e60231366098b4d459d6240
Leonid Belyi <leonid@ac93.org>
2016-08-01 11:19:48 -0400

    fixed dumb copying errors

--- scripts/directionless_mover.py
+++ scripts/directionless_mover.py
@@ -1,42 +1,42 @@
-#!/usr/bin/env/python
+#!/usr/bin/env python
 
+import numpy as np
+import math
 import rospy
-from sensor_msgs.msg import *
-from ackermann_msgs.msg import *
+from sensor_msgs.msg import LaserScan
 from std_msgs.msg import *
+from ackermann_msgs.msg import *
 
-class FindEmptySpace:
-	def __init__(self):
-		self.node_name = "find_empty_space"
-		self.header = std_msgs.msg.Header()
-		self.sub_scan = rospy.Subscriber("/scan", LaserScan, self.empty_space)
-		self.pub_drive = rospy.Publisher("/vesc/ackermann_cmd_mux/input/navigation", AckermannDriveStamped, queue_size = 1)
+class WinterDodgeNode:
+    def __init__(self):
+        self.node_name = "winter_dodge_node"
+        self.K_energy = -0.12
+        self.drive_pub = rospy.Publisher("/vesc/ackermann_cmd_mux/input/navigation", AckermannDriveStamped, queue_size = 1)
+        self.scan_sub = rospy.Subscriber("/scan", LaserScan, self.laser_callback)
+        self.header = std_msgs.msg.Header()
 
-		rospy.loginfo("Initialized")
-	def empty_space(self, msg):
-		drivemsg = AckermannDriveStamped(self.header, AckermannDrive(speed=0.0, steering_angle=0.0))
-		drivemsg.header = self.header
-		drivemsg.drive.speed = 0.7
-		Pd = 1.0
-		#Scan 120 deg in front (from 300 to 780)
-		pts = msg.ranges[420:660]
-		max_index = pts.index(max(pts)) + 420
-		max_center = 540
-		
-		error_index = max_index - max_center
-		error_index = float(error_index)
-		
-		steer_output = (error_index / 120) * Pd 
-		print(max_index)
-		print(steer_output)
-		
-		drivemsg.drive.steering_angle = steer_output
-		self.pub_drive.publish(drivemsg)
-if __name__ == '__main__':
-	rospy.init_node('find_empty_space')
-	node = FindEmptySpace()
-	rospy.spin()
- 
-		
-			
-			
+    def laser_callback(self, msg):
+
+        x_force_total = -self.K_energy / 0.025**2
+        y_force_total = 0
+        for i in range(180, 900):
+            angle = math.radians((i - 540)/4)
+            dist = msg.ranges[i]
+            
+            force = self.K_energy / dist**2
+
+            x_force = math.cos(angle) * force
+            y_force = math.sin(angle) * force
+
+            x_force_total = x_force_total + x_force
+            y_force_total = y_force_total + y_force
+
+        speed = 0.005 * math.sqrt(x_force_total**2 + y_force_total**2) * np.sign(x_force_total)
+        steering_angle = 1.0 * math.atan2(y_force_total, x_force_total) * np.sign(x_force_total)
+        print(x_force_total, y_force_total, speed, steering_angle)
+        self.drive_pub.publish(AckermannDriveStamped(self.header, AckermannDrive(speed = speed, steering_angle = steering_angle)))
+
+if __name__ == "__main__":
+    rospy.init_node("winter_dodge_node")
+    node = WinterDodgeNode()
+    rospy.spin()
