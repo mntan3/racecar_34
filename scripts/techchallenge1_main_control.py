69323275682220d02b23269ff0949a52d2675922
Michelle Tan <mtan123456789@gmail.com>
2016-08-01 23:56:05 -0400

    Fixed syntax

--- scripts/techchallenge1_main_control.py
+++ scripts/techchallenge1_main_control.py
@@ -2,18 +2,20 @@
 import rospy
 from ackermann_msgs.msg import AckermannDriveStamped
 from std_msgs.msg import *
+from sensor_msgs.msg import *
+from team34.msg import BlobDetections
 
-class MainControl:
+class MainControl1:
     #Other nodes will be controlled from here
     def __init__(self):
         self.node_name = "tech_challenge_1_main_control_node"
         self.ableToControl = True
 
         #rospy.Subscriber("/transition", String, self.transition_callback)
-        self.joy_control = rospy.Subscriber("/vesc/joy", Joy, joy_buttons)
-        self.sub_blob_detection = rospy.Subscriber("blob_detections", BlobDetections, blob_cb)
-        self.sub_winter = rospy.Subscriber("main_drive", AckermannDriveStamped, movement_cb)
-        self.sub_safety = rospy.Subscriber("safety", AckermannDriveStamped, safety_cb)
+        #self.joy_control = rospy.Subscriber("/vesc/joy", Joy, joy_buttons)
+        self.sub_blob_detection = rospy.Subscriber("blob_detections", BlobDetections, self.blob_cb)
+        self.sub_winter = rospy.Subscriber("main_drive", AckermannDriveStamped, self.movement_cb)
+        self.sub_safety = rospy.Subscriber("safety", AckermannDriveStamped, self.safety_cb)
 
         #Add publisher for every node that is to be controlled
         self.drive_pub = rospy.Publisher("/vesc/ackermann_cmd_mux/input/navigation", AckermannDriveStamped, queue_size=1)
@@ -25,7 +27,8 @@ class MainControl:
     #def transition_callback(self, msg):
         #if msg.data == "follow left" or msg.data == "follow right" or msg.data == "stop follow"
             #self.wall_pub.publish(msg)
-
+    def blob_cb(self, msg): 
+        pass
     def movement_cb(self, msg):
         if self.ableToControl:
             self.sub_winter.publish(msg)
@@ -40,6 +43,6 @@ class MainControl:
 
 
 if __name__=="__main__":
-    rospy.init_node("MainControl")
-    node = Control()
+    rospy.init_node("tech_challenge_1_main_control_node")
+    node = MainControl1()
     rospy.spin()
