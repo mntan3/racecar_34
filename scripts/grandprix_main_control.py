b96ef909d362637d3897713ef79a624d7d956703
Michelle Tan <mtan123456789@gmail.com>
2016-08-01 21:56:20 -0400

    Fixed minor syntax stuff

--- scripts/grandprix_main_control.py
+++ scripts/grandprix_main_control.py
@@ -2,12 +2,13 @@
 import rospy
 from ackermann_msgs.msg import AckermannDriveStamped
 from std_msgs.msg import *
+from sensor_msgs.msg import *
 
-class mainControl:
+class MainControl:
     #Other nodes will be controlled from here
     def __init__(self):
         rospy.Subscriber("/transition", String, self.transition_callback)
-        self.joy_control = rospy.Subscriber("/vesc/joy", Joy, joy_buttons)
+        #self.joy_control = rospy.Subscriber("/vesc/joy", Joy, joy_buttons)
 
         #Add publisher for every node that is to be controlled
         self.wall_pub = rospy.Publisher("/wallfollow", String, queue_size=1)
@@ -17,7 +18,7 @@ class mainControl:
 
     #When message recieved from /transition topic, start the appropriate node by publishing a message 
     def transition_callback(self, msg):
-        if msg.data == "follow left" or msg.data == "follow right" or msg.data == "stop follow"
+        if msg.data == "follow left" or msg.data == "follow right" or msg.data == "stop follow":
             self.wall_pub.publish(msg)
 
     #Optional function to start programs using the joystick
@@ -26,5 +27,5 @@ class mainControl:
 
 if __name__=="__main__":
     rospy.init_node("MainControl")
-    node = Control()
+    node = MainControl()
     rospy.spin()
